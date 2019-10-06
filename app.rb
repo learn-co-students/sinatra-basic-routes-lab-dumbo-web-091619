a1b750530df55d5946521349d10983c78ab0ace3
Gaida Escobar <gescobar129@gmail.com>
2019-10-06 17:47:46 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Gaida"
+    end
+
+    get '/hometown' do
+        "My hometown is Paterson"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is too many to choose from"
+    end
+
 end
